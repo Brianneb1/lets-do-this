af66f8d15437f20803873296dbb1074cbd846591
Brianne Bender <s529231@nwmissouri.edu>
2019-07-23 20:58:59 -0500

    trying fun stuff

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/brandon")
 def salvador():
     return "Hello, Brandon"
+
+@app.route("/fancy")
+def fancy():
+    return render_template("fancy.html")
 
 
 if __name__ == "__main__":
